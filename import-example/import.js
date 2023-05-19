dd5777cfb7255c17dcaa58f30a7ace4b6cd20459
JulianXander <julian.xander@ymail.com>
2023-05-19 23:05:02 +0200

    build to out

--- import-example/import.js
+++
@@ -1,6 +0,0 @@
-const { _callFunction, _checkType, _createFunction, log } = require("./runtime");
-const valueImport = require("./somefile");
-exports.valueImport = valueImport;
-_callFunction(log, [
-valueImport.someKey,
-])
--- import-example/somefile.js
+++
@@ -1,4 +0,0 @@
-const { _callFunction, _checkType, _createFunction, log } = require("./runtime");
-module.exports = {
-someKey: 5,
-}
