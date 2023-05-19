4192836ee6a68aa72c11357d021bd61a6fe5adeb
JulianXander <julian.xander@ymail.com>
2023-05-19 20:45:07 +0200

    build to out

--- http-request/http-request.js
+++
@@ -1,8 +0,0 @@
-const { _branch, _callFunction, _checkType, _createFunction, _checkDictionaryType, deepEquals, BuiltInTypeBase, AnyType, BooleanType, IntegerType, FloatType, StringType, DictionaryLiteralType, TupleType, StreamType, FunctionType, ParameterReference, TypeType, IntersectionType, UnionType, ComplementType, TypeOfType, Any, _Boolean, Float, Integer, NonZeroInteger, _String, _Error, Type, equal, modulo, subtract, subtractFloat, sum, sumFloat, complete, subscribe, httpTextRequest$, timer$, log, repeat, runJs } = require("./runtime");
-module.exports = _callFunction(subscribe, [
-_callFunction(httpTextRequest$, [
-`https://api.ipify.org/?format=json`,
-`get`,
-]),
-log,
-])
