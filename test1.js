e0f52140130600b409f08023be689e13dc06ebb7
JulianXander <julian.xander@ymail.com>
2023-05-20 18:35:16 +0200

    cleanup

--- test1.js
+++
@@ -1,6 +0,0 @@
-const { _branch, _callFunction, _checkType, _createFunction, _checkDictionaryType, deepEquals, BuiltInTypeBase, AnyType, BooleanType, IntegerType, FloatType, StringType, TupleType, DictionaryLiteralType, StreamType, FunctionType, ParameterReference, TypeType, IntersectionType, UnionType, ComplementType, TypeOfType, Any, _Boolean, Float, Integer, NonZeroInteger, _String, _Error, Type, equal, modulo, subtract, subtractFloat, sum, sumFloat, parseJson, forEach, complete, subscribe, httpTextRequest$, timer$, log, repeat, runJs } = require("./runtime");
-const t = _callFunction(Without, [
-_Boolean,
-true,
-]);
-exports.t = t;
