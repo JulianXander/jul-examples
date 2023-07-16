d6fb45529702f03d44ef50db60c47d8f8eb8cf99
JulianXander <julian.xander@ymail.com>
2023-07-17 01:08:00 +0200

    test fns

--- import-example/js-file.js
+++ import-example/js-file.js
@@ -1,2 +1,4 @@
+export function testfn1(par1) { log(par1) };
+export const testfn2 = (par1) => log(par1);
 export const test = 'hello js';
 export const test2 = 4;
--- import-example/ts-file.ts
+++ import-example/ts-file.ts
@@ -1,3 +1,5 @@
 export type MyType = string | number;
 export const test: MyType = 'hello ts';
 export const test2: MyType = 4;
+export function testfn1(par1: string) { console.log(par1) };
+export const testfn2 = (par1: string) => console.log(par1);
